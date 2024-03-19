930d3dc87d7f36799bf45d49083a4daae63f4d50
Pedro Camboim <pedrocamboim@klever.io>
2024-03-19 11:29:29 -0300

    feat: plot grouped by function

--- plot.py
+++ plot.py
@@ -1,3 +1,4 @@
+import matplotlib.cm as cm
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -10,7 +11,11 @@ def plot(xbest, fbest, objective_function, dom1, dom2):
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
     ax.plot_surface(X, Y, Z, cmap='jet', rstride=10, cstride=10, alpha=0.6)
-    ax.scatter(xbest[0], xbest[1], fbest, c='r', marker='o')
+    
+    colors = cm.rainbow(np.linspace(0, 1, len(xbest)))
+    
+    for i in range(len(xbest)):
+        ax.scatter(xbest[i][0], xbest[i][1], fbest[i], color=colors[i], marker='o')
     plt.tight_layout()
     plt.show()
 
